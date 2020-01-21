518efc900d055d230dd83241e9f59cd2f40964f8
MC_Atom <thepnkprkr@gmail.com>
2020-01-21 15:24:18 -0800

    Made code much more effitient by reworking everything

---
+++ helpFunctions/randRun.py
@@ -0,0 +1,19 @@
+import random
+
+x = input("how long are rows? ")
+y = input("how long are columbs? ")
+
+for i in range(int(x)):
+    for d in range(int(y)):
+        ra = random.randint(0,2)
+        if ra == 0:
+            print('0'),
+        elif ra == 1:
+            print('8'),
+        elif ra == 2:
+            print('16'),
+            
+        if d == (y-1):
+            print('')
+        else:
+            print(','),
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import { makeTile } from './tileLoader.js';
-import { playerAndCameraCreate, playerMove } from './movement.js';
-
 var config = {
-    type: Phaser.CANVAS,
-    width: 400,
-    height: 300,
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#2d2d2d',
+    parent: 'phaser-example',
+    pixelArt: true,
     physics: {
         default: 'arcade',
         arcade: {
@@ -14,51 +14,148 @@ var config = {
     scene: {
         preload: preload,
         create: create,
-        update: update,
-        //render:render
+        update: update
     }
 };
 
-var player;
-var graphics;
-var cursors;
-var moveCam = false;
-
-
 var game = new Phaser.Game(config);
-
-function preload () {
-    this.load.setBaseURL('');
-    this.load.spritesheet('grass', 'assets/grass_tiles.png', { frameWidth: 32, frameHeight: 32 });
-    this.load.spritesheet('playerImages', 'assets/dude1.png', { frameWidth: 32, frameHeight: 64 });
-}
-
+var map;
 var cursors;
+var debugGraphics;
+var helpText;
+var player;
+var showDebug = false;
 
-function create () {
-    
-    var logo;
-    var tile = this.physics.add.staticGroup();
-    
-    var matrixIn = [["TR100 24"],["TR100 24"],["DR100"]];
-    makeTile(tile,matrixIn,"grass",24,1);
-    
-
-    var matrixIn = [["DR8",1,2,3,"DR 89"],["DR8",17,18,19,"DR89"],["TR100 2"],["CD",24,32,40],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"],["DR50"]];
-    
-    makeTile(tile,matrixIn,"grass",0,1);
-    
-    player = playerAndCameraCreate ('playerImages',this)
-
+function preload ()
+{
+    this.load.image('tiles', 'assets/grass_tiles.png');
+    this.load.tilemapCSV('map', 'assets/Level1.csv');
+    this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 });
 }
 
-function update() { 
-    
-    var cam = this.cameras.main;
+function create ()
+{
+    map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
+    var tileset = map.addTilesetImage('tiles');
+    var layer = map.createStaticLayer(0, tileset, 0, 0);
+
+    //  This isn't totally accurate, but it'll do for now
+    map.setCollisionBetween(54, 83);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 3, end: 5 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 6, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'up',
+        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 11 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'down',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    player = this.physics.add.sprite(50, 100, 'player', 1);
+
+    // Set up the player to collide with the tilemap layer. Alternatively, you can manually run
+    // collisions in update via: this.physics.world.collide(player, layer).
+    this.physics.add.collider(player, layer);
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(player);
+
+    debugGraphics = this.add.graphics();
+
+    this.input.keyboard.on('keydown_C', function (event) {
+        showDebug = !showDebug;
+        drawDebug();
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
 
-    playerMove(player,cursors);
-    
-    
-    
+    helpText = this.add.text(16, 16, getHelpMessage(), {
+        fontSize: '18px',
+        fill: '#ffffff'
+    });
+    helpText.setScrollFactor(0);
+}
+
+function update (time, delta)
+{
+    player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (cursors.left.isDown)
+    {
+        player.body.setVelocityX(-100);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.setVelocityX(100);
+    }
+
+    // Vertical movement
+    if (cursors.up.isDown)
+    {
+        player.body.setVelocityY(-100);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.body.setVelocityY(100);
+    }
+
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (cursors.left.isDown)
+    {
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.anims.play('right', true);
+    }
+    else if (cursors.up.isDown)
+    {
+        player.anims.play('up', true);
+    }
+    else if (cursors.down.isDown)
+    {
+        player.anims.play('down', true);
+    }
+    else
+    {
+        player.anims.stop();
+    }
+}
+
+function drawDebug ()
+{
+    debugGraphics.clear();
+
+    if (showDebug)
+    {
+        // Pass in null for any of the style options to disable drawing that component
+        map.renderDebug(debugGraphics, {
+            tileColor: null, // Non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 200), // Colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Colliding face edges
+        });
+    }
+}
+
+function getHelpMessage ()
+{
+    return 'Arrow keys to move.' +
+        '\nPress "C" to toggle debug visuals: ' + (showDebug ? 'on' : 'off');
 }
