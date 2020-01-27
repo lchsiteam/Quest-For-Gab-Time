9b46c6a54d5b8206607c72b5b45e2f064ae1f0e1
MC-Atom <thepnkprkr@gmail.com>
2020-01-27 14:05:18 -0800

    Got to be honest, I did a lot and I don't remember most of what I did since last update, but I added doors and switched the tile map and added a sprint key, it was a good set of updates

--- src/scene1.js
+++ src/scene1.js
@@ -12,9 +12,11 @@ export class Scene1 extends Phaser.Scene {
      preload ()
 {
     this.load.image('tiles', 'assets/grass_tiles.png');
-    this.load.image('doors', 'assets/doors.png');-
-    this.load.tilemapCSV('map', 'assets/Level1layer2.csv');
+    this.load.image('concrete', 'assets/concrete_tiles.png');
+    this.load.image('doors', 'assets/doors.png');
+    this.load.tilemapCSV('map', 'assets/level1layer2.csv');
     this.load.tilemapCSV('layer1', 'assets/level1layer1.csv');
+    this.load.tilemapCSV('layer15', 'assets/level1layer15.csv');
     this.load.tilemapCSV('doorCSV', 'assets/Level1Doors.csv');
     this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 });
 }
@@ -27,32 +29,38 @@ export class Scene1 extends Phaser.Scene {
     }
 
     var layer1map;
+    var layer15map
     var map;
     var doorsLayerMap;
     this.Keystrokes = [];
     
-    layer1map = this.make.tilemap({ key: 'layer1', tileWidth: 32, tileHeight: 32 });
+    layer1map = this.make.tilemap({ key: 'layer1', tileWidth: 32, tileHeight: 32 });  //dark grass
     var tileset1 = layer1map.addTilesetImage('tiles');
     var layer1 = layer1map.createStaticLayer(0, tileset1, 0, 0);
-    layer1map.setCollisionBetween(5, 6);
+    //layer1map.setCollisionBetween(5, 6);
+    
+    layer15map = this.make.tilemap({ key: 'layer15', tileWidth: 32, tileHeight: 32 });  //concrete
+    var tileset15 = layer15map.addTilesetImage('concrete');
+    var layer15 = layer15map.createStaticLayer(0, tileset15, 0, 0);
+    //layer1map.setCollisionBetween(5, 6);
 
-    map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
+    map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });   //grass
     var tileset = map.addTilesetImage('tiles');
     var layer = map.createStaticLayer(0, tileset, 0, 0);
-    //map.setCollisionBetween(5, 6);
+    map.setCollisionBetween(70, 70);
     
-    doorsLayerMap = this.make.tilemap({ key: 'doorCSV', tileWidth: 32, tileHeight: 32 });
+    doorsLayerMap = this.make.tilemap({ key: 'doorCSV', tileWidth: 32, tileHeight: 32 });  //doors
     var doorMap = doorsLayerMap.addTilesetImage('doors');
     var doorLayer = doorsLayerMap.createStaticLayer(0, doorMap, 0, 0);
-    doorsLayerMap.setCollisionBetween(0, 10);
+    doorsLayerMap.setCollisionBetween(7, 10);
     
     
     this.Keystrokes.keyZ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
 
-    this.player = this.physics.add.sprite(463, 60, 'player', 1);
+    this.player = this.physics.add.sprite(495, 92, 'player', 1);
 
     
-    this.physics.add.collider(this.player, layer1);
+    this.physics.add.collider(this.player, layer);
     this.physics.add.collider(this.player, doorLayer, this.startScene2, null, this);
 
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
