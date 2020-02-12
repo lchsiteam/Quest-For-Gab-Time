1086d155d7c83ee5ece194bb2b39f408a6879d7e
epicodaboss <epicean@gmail.com>
2020-02-12 15:19:54 -0800

    Health Bar

--- src/Health.js
+++ src/Health.js
@@ -14,16 +14,48 @@ preload ()
     
 }
 
- create ()
+
+
+create ()
 {
+    var graphics;
+    graphics = this.add.graphics();
+
+    graphics.fillStyle(0xFF0000, 1);
+
+    //  32px radius on the corners
+    graphics.fillRect(32, 535, 300, 50);
+
+    
+    
+    var size = 300
+    setInterval( () => {
+
+       
+
+            graphics.fillStyle(0x00000, 1);
+            graphics.fillRect(32, 535, 300, 50);
+            if (size >= 10){
+            graphics.fillStyle(0xFF0000, 1);
+            graphics.fillRect(32, 535, size, 50);
+            }
+            else{
+                graphics.visible = false
+            }
+
+        size -= 10
+
+
+    }, 1000)
+
+    //  Using an object to define a different radius per corner
     
 }
-
  update ()  {
      
-     
+    
      
 }
-    
+
     
 }
