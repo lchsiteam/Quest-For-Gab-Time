3299c96f500d1da33266fc1ff0d4989827bb4661
epicodaboss <epicean@gmail.com>
2020-02-21 10:57:00 -0800

    Added mana bar and also a toggle between health and mana + health

--- src/Health.js
+++ src/Health.js
@@ -8,7 +8,9 @@ export class Health extends Phaser.Scene {
 
 preload ()
 {
+    this.load.image('healthAndManaBar', 'assets/Images/HealthBarV3.png');
     this.load.image('healthBar', 'assets/Images/HealthBar.png');
+    
 }
 
 init (data)
@@ -17,6 +19,7 @@ init (data)
         'use strict';
 
         this.PASSING_OBJ = data;
+        var mana = 100
     }
 
 create ()
@@ -24,41 +27,104 @@ create ()
     
 
     
-    this.add.image(208, 560, 'healthBar');
+    //this.add.sprite(208, 560,  'healthBar');
+    
     
     
     
     
 }
 update ()  {
-        var graphics;
-    var size = 300;
+    var graphics;
+    var healthSize = 300;
+    var manaSize = 300;
+
+    var maxMana = 100 //Make Global Vars
+    var mana; // Make Global Vars
+
+    var manaEnabled = true //make global as well
+
+  
+
+
 
     graphics = this.add.graphics();
     graphics.fillStyle(0xFF0000, 1);
 
-    //  32px radius on the corners
-    graphics.fillRect(82, 535, 300, 50);
+
         if (this.PASSING_OBJ.playerData.health < 0) {
             this.PASSING_OBJ.playerData.health = 0; 
         } else if (this.PASSING_OBJ.playerData.health > this.PASSING_OBJ.playerData.maxHealth) {
                 this.PASSING_OBJ.playerData.health = this.PASSING_OBJ.playerData.maxHealth;
         }
+
+
+       
         
 
-       size = (this.PASSING_OBJ.playerData.health/this.PASSING_OBJ.playerData.maxHealth) * 300
+       healthSize = (this.PASSING_OBJ.playerData.health/this.PASSING_OBJ.playerData.maxHealth) * 300
        //this.PASSING_OBJ.playerData.health -= 1
 
-        graphics.fillStyle(0x00000, 1);
-        graphics.fillRect(82, 534, 300, 51);
+        console.log(this.PASSING_OBJ.playerData.health )
+       
+       mana = this.PASSING_OBJ.playerData.health  - 200
+      
+       
+       if (mana < 0) {
+        mana = 0; 
+    } else if (mana > maxMana) {
+        mana = maxMana;
+    }
 
-            graphics.fillStyle(0xFF0000, 1);
-            graphics.fillRect(82, 534, size, 51);
+    manaSize = (mana/maxMana) * 300
+       console.log(manaSize)
+       console.log(maxMana)
+       console.log(mana)
+      
+
+
+       if (manaEnabled){
+        graphics.fillStyle(0x00000, 1);
+        graphics.fillRect(82, 534, 300, 26);
+
+        graphics.fillStyle(0xFF0000, 1);
+        graphics.fillRect(82, 534, healthSize, 26);
+
+        graphics.fillStyle(0x00000, 1);
+        graphics.fillRect(82, 560, 300, 26);
+
+        graphics.fillStyle(0x0000FF, 1);
+        graphics.fillRect(82, 560, manaSize, 26);
+
+       }
+
+       else 
+       {
+        graphics.fillStyle(0x00000, 1);
+        graphics.fillRect(82, 534, 300, 52);
+
+        graphics.fillStyle(0xFF0000, 1);
+        graphics.fillRect(82, 534, healthSize, 52);
+       }
+
     
     
-    //console.log(size)
- 
-        
+
+ /*setTimeout(() => {
+
+
+
+
+ })*/
+
+    if (manaEnabled)
+    {
+        this.add.image(208, 560,  'healthAndManaBar');
+    }
+    else
+    {
+        this.add.image(208, 560,  'healthBar');
+    }
 }
     
 }
