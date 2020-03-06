132b7605ac3112d0d583fec1bfc490e2a741e4fc
Quincy <55165281+qstrange23@users.noreply.github.com>
2020-03-05 17:57:13 -0800

    Made scene scale & center

--- src/main.js
+++ src/main.js
@@ -1,14 +1,31 @@
 import { init } from './init.js';
 import { Scene1 } from './scene1.js';
 import { Scene2 } from './scene2.js';
-import { Scene3 } from './scene3.js';
 import { gui } from './gui.js';
 
+if (window.innerWidth > window.innerHeight) {
+    var h = (window.innerHeight - (window.innerHeight % 3));
+    var w = (h * (4/3));
+} else {
+    var w = (window.innerWidth - (window.innerWidth % 4));
+    var h = (w * (3/4))
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > window.innerHeight) {
+        var h = (window.innerHeight - (window.innerHeight % 3));
+        var w = (h * (4/3));
+    } else {
+        var w = (window.innerWidth - (window.innerWidth % 4));
+        var h = (w * (3/4))
+    }
+    game.scale.resize(w, h);
+});
 
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: w,
+    height: h,
     backgroundColor: '#2d2d2d',
     parent: 'phaser-example',
     pixelArt: true,
@@ -18,8 +35,11 @@ var config = {
             gravity: { y: 0 }
         }
     },
-    scene: [init,Scene1,Scene2,Scene3,gui]
+    scene: [init,Scene1,Scene2,gui]
     
 };
 
 var game = new Phaser.Game(config);
+game.scale.pageAlignHorizontally = true;
+game.scale.pageAlignVertically = true;
+
