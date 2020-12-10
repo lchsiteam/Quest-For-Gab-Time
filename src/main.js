9876ee834fce324746eeb026d1491823e6b8e95e
Quincy <55165281+EnderLord0042@users.noreply.github.com>
2020-12-09 18:32:26 -0800

    Fix scaling

--- src/main.js
+++ src/main.js
@@ -7,32 +7,16 @@ import { Scene5 } from './scenes/scene5.js';
 import { gui } from './gui.js';
 import { pause } from './pause.js';
 
-if ((window.innerWidth/4) > (window.innerHeight/3)) {
-    var h = (window.innerHeight - (window.innerHeight % 3));
-    var w = (h * (4/3));
-} else {
-    var w = (window.innerWidth - (window.innerWidth % 4));
-    var h = (w * (3/4))
-}
-
-window.addEventListener('resize', () => {
-    if ((window.innerWidth/4) > (window.innerHeight/3)) {
-        var h = (window.innerHeight - (window.innerHeight % 3));
-        var w = (h * (4/3));
-    } else {
-        var w = (window.innerWidth - (window.innerWidth % 4));
-        var h = (w * (3/4))
-    }
-    game.scale.resize(w, h);
-});
-
 var config = {
     type: Phaser.AUTO,
-    width: w,
-    height: h,
     backgroundColor: '#2d2d2d',
-    parent: 'phaser-example',
     pixelArt: true,
+    scale: {
+       parent: 'gameDiv',
+       mode: Phaser.Scale.FIT,
+       width: 800,
+       height: 600
+    },
     input: {
         gamepad: true
     },
@@ -50,5 +34,3 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-game.scale.pageAlignHorizontally = true;
-game.scale.pageAlignVertically = true;
